8d9f89633ecbcf70df4676298024d74ab0466db6
nihatinha <nqarayev5@std.beu.edu.az>
2024-10-25 11:58:53 +0400

    First

---
+++ src/main/java/com/example/sdf11/Sdf11Application.java
@@ -0,0 +1,13 @@
+package com.example.sdf11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdf11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdf11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sdf11/Sdf11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdf11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdf11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
